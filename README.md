2a6f95a24176f2d00da0df058dcc62402ba8c52c
Adam Field <adamjfield@gmail.com>
2021-10-22 21:40:16 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work Day Scheduler Starter Code
+
+## Description
+* Simple daily schedule app that allows user to list this they need to do that day by hour from 9 a.m. to 5 p.m. 
+* The hour blocks will be gray for past hours, red for current hour, and green for future hours. 
+* When user adds item to hour block and hits the save button the information will be saved to local storage and reload if page is closed and reopened. 
+* If users wishes to there is a clear schedule button that clear all data from local storage and reload page to give user an empty schedule.git
+
+### Languages Used 
+HTML, CSS, JQuery, Bootstrap, and Moment.js
+
+## Screen Shot
+![screenshot](./assets/images/Work-Day-Screenshot.jpg)
+
+## Link to Application
+
+https://adamjfield.github.io/WorkDayScheduler-AdamField/
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,43 +14,41 @@ $(document).ready(function () {
   $(".saveBtn").on("click", function () {
     var time = $(this).parent().attr("id");
     var value = $(this).siblings(".description").val();
-    var savedMsg = document.createElement
+    var savedMsg = document.createElement;
     localStorage.setItem(time, value);
   });
 
   function hrUpdate() {
-// variable to get current number of hours from moment.js
-var currentHour = moment().format("HH").toString();
-console.log(currentHour);
-// loop over time block to identify past future present
-$(".time-block").each(function () {
-  var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  console.log(blockHour);
-  if (blockHour < currentHour) {
-    $(this).addClass("past");
-  } else if (blockHour == currentHour) {
-    $(this).removeClass("past");
-    $(this).addClass("present");
-  } else {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-    $(this).addClass("future");
+    // variable to get current number of hours from moment.js
+    var currentHour = moment().format("HH").toString();
+    // loop over time block to identify past future present
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour == currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
   }
-});
-}
 
-hrUpdate();
+  hrUpdate();
 
-// load any save data from localStorage
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  // load any save data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
 
 // button to clear localStorage
