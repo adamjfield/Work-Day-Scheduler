a975d49d58f99db1fd3ffde464d75473f0b1375f
Adam Field <adamjfield@gmail.com>
2021-10-22 20:26:20 -0500

    add pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// variables for time
+
+// display current day on page
+
+// listen for save button click
+
+// variable to get current number of hours from moment.js
+
+// loop over time block to identify past future present
+
+
+// load any save data from localStorage
+
+// button to clear local storage
